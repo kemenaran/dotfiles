42aa3a2ac0eb476a420de737b7405c3799b8988c
beta.gouv.fr <beta.gouv.fr@poire.local>
2016-12-06 17:28:38 +0100

    Add installation script

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -ex
+# Symlink dotfiles to their location
+
+DOT_ROOT=$(cd `dirname "$0"`; pwd)
+
+_confirm() {
+	local dest_file="$1"
+	confirmed="no"
+
+	if [ -e "$dest_file" -o -L "$dest_file" ]
+	then
+		confirmed="no";
+	else
+		confirmed="yes";
+		return
+	fi
+
+	while true; do
+		echo "File '$dest_file' already exists."
+	    read -p "Do you want to overwrite it with the dotfile? [y/n] " yn
+	    case $yn in
+	        [Yy]* ) confirmed="yes";  break;;
+	        [Nn]* ) confirmed="no"; break;;
+	        * ) echo "Please answer yes or no.";;
+	    esac
+	done
+}
+
+_install() {
+	local src_file="$1"
+	local dest_file="$2"
+	_confirm "$dest_file"
+	if [ "$confirmed" == "yes" ]; then
+		[ -e "$dest_file" -o -L "$dest_file" ] && rm "$dest_file"
+		ln -s "$src_file" "$dest_file"
+		echo "Installed $dest_file"
+	fi
+}
+
+_install "$DOT_ROOT/git/gitconfig" "$HOME/.gitconfig"
+_install "$DOT_ROOT/shell/profile" "$HOME/.profile"
+_install "$DOT_ROOT/sublime-text/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+_install "$DOT_ROOT/sublime-text/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
